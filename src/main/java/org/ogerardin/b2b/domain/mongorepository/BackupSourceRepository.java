fff7d00e6101b8482be56ec3883b89e2b152bff8
oge <olivier.gerardin@fundsquare.net>
2017-10-17 16:41:42 +0200

    Implemented RestBackupSourcesController, RestBackupTargetsController

--- src/main/java/org/ogerardin/b2b/domain/mongorepository/BackupSourceRepository.java
+++ src/main/java/org/ogerardin/b2b/domain/mongorepository/BackupSourceRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface BackupSourceRepository extends MongoRepository<BackupSource, String> {
 
+    BackupSource findByName(String name);
+
 }
--- src/main/java/org/ogerardin/b2b/domain/mongorepository/BackupTargetRepository.java
+++ src/main/java/org/ogerardin/b2b/domain/mongorepository/BackupTargetRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface BackupTargetRepository extends MongoRepository<BackupTarget, String> {
 
+    BackupTarget findByName(String name);
+
 }
