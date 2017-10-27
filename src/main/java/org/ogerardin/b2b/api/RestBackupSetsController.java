d13c02fce42a0fd9855f73a8549ce88e665f6bb8
ogerardin@yahoo.com <bebe081060>
2017-10-27 12:27:10 +0200

    added URL mapping for files

--- src/main/java/org/ogerardin/b2b/api/RestBackupSetsController.java
+++ src/main/java/org/ogerardin/b2b/api/RestBackupSetsController.java
@@ -2,13 +2,18 @@ package org.ogerardin.b2b.api;
 
 import org.ogerardin.b2b.domain.BackupSet;
 import org.ogerardin.b2b.domain.mongorepository.BackupSetRepository;
+import org.ogerardin.b2b.storage.StorageService;
+import org.ogerardin.b2b.storage.StorageServiceFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/backupsets")
@@ -16,9 +21,12 @@ public class RestBackupSetsController {
 
     private final BackupSetRepository backupSetRepository;
 
+    private final StorageServiceFactory storageServiceFactory;
+
     @Autowired
-    public RestBackupSetsController(BackupSetRepository backupSetRepository) {
+    public RestBackupSetsController(BackupSetRepository backupSetRepository, @Qualifier("gridFsStorageServiceFactory") StorageServiceFactory storageServiceFactory) {
         this.backupSetRepository = backupSetRepository;
+        this.storageServiceFactory = storageServiceFactory;
     }
 
     @GetMapping
@@ -32,5 +40,14 @@ public class RestBackupSetsController {
     public BackupSet get(@PathVariable String id) {
         return backupSetRepository.findOne(id);
     }
+
+    @GetMapping("/{id}/files")
+    public Path[] getFiles(@PathVariable String id) {
+        BackupSet backupSet = backupSetRepository.findOne(id);
+        StorageService storageService = storageServiceFactory.getStorageService(backupSet.getId());
+        List<Path> paths = storageService.loadAll().collect(Collectors.toList());
+        return paths.toArray(new Path[paths.size()]);
+    }
+
 
 }
