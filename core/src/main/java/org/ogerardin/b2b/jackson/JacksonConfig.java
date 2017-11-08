ab81a4a2d10bd222a52b8e96975250e23c80b662
oge <olivier.gerardin@fundsquare.net>
2017-11-08 11:56:13 +0100

    Register Jackson serializers/deserializers on client side

--- core/src/main/java/org/ogerardin/b2b/jackson/JacksonConfig.java
+++ core/src/main/java/org/ogerardin/b2b/jackson/JacksonConfig.java
@@ -2,22 +2,29 @@ package org.ogerardin.b2b.jackson;
 
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.nio.file.Path;
 
 /**
  * Custom configuration for Jackson (JSON serialization)
  */
-//@Configuration
+@Configuration
 public class JacksonConfig {
 
     /**
      * Provides a {@link com.fasterxml.jackson.databind.Module} that adds custom
-     * serializers and deserializers.
+     * serializers and deserializers. This bean will be picked up by Spring and used to configure its
+     * {@link com.fasterxml.jackson.databind.ObjectMapper}.
+     *
      */
-//    @Bean
+    @Bean
     public static Module customSerializationModule() {
         SimpleModule module = new SimpleModule();
+        // Jackson handles {@link Path} serialization/deserialization correctly out of the box, but it wraps
+        // the path in a URI. Here we just provide a serializer and deserializer that handle the native form
+        // of the path.
         module.addSerializer(Path.class, new PathSerializer());
         module.addDeserializer(Path.class, new PathDeserializer());
         return module;
--- ui/src/main/java/B2BfacadeRest.java
+++ ui/src/main/java/B2BfacadeRest.java
@@ -1,21 +1,31 @@
 import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.ogerardin.b2b.domain.BackupSet;
 import org.ogerardin.b2b.jackson.JacksonConfig;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 public class B2BfacadeRest implements B2Bfacade {
 
+    private static RestTemplate restTemplate = new RestTemplate();
+
     static {
-        ObjectMapper objectMapper = new ObjectMapper();
+        // register custom serializers/deserializers from the core project
         Module module = JacksonConfig.customSerializationModule();
-        objectMapper.registerModule(module);
+        registerModule(restTemplate, module, MappingJackson2HttpMessageConverter.class);
     }
 
     @Override
     public BackupSet[] getBackupSets() {
-        RestTemplate restTemplate = new RestTemplate();
         BackupSet[] backupSets = restTemplate.getForObject("http://localhost:8080/api/backupsets", BackupSet[].class);
         return backupSets;
+    }
+
+    private static void registerModule(RestTemplate restTemplate, Module module,
+                                       Class<? extends AbstractJackson2HttpMessageConverter> converterClass) {
+        restTemplate.getMessageConverters().stream()
+                .filter(converterClass::isInstance)
+                .map(converterClass::cast)
+                .forEach(mc -> mc.getObjectMapper().registerModule(module));
     }
 }
