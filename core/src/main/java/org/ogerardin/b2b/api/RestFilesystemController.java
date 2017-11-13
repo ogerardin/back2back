5a00570de1b512b68b8516962915e0bbb2306d77
ogerardin@yahoo.com <bebe081060>
2017-11-13 13:05:12 +0100

    Modified RestFilesystemController to return directory flag

--- core/src/main/java/org/ogerardin/b2b/api/RestFilesystemController.java
+++ core/src/main/java/org/ogerardin/b2b/api/RestFilesystemController.java
@@ -1,5 +1,6 @@
 package org.ogerardin.b2b.api;
 
+import lombok.Data;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -7,17 +8,37 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * This controller is meant to interact with the local filesystem, to allow browsing
+ * files from a remote UI.
+ */
 @RestController
 @RequestMapping("/api/filesystem")
 public class RestFilesystemController {
 
+    @Data
+    public static class FSItem {
+        Path path;
+        boolean directory;
+
+        FSItem() {}
+
+        FSItem(Path path, boolean directory) {
+            this.path = path;
+            this.directory = directory;
+        }
+    }
+
     @GetMapping()
-    Path[] list(@RequestParam(value = "dir", required = false) String dir) throws IOException {
+    FSItem[] list(@RequestParam(value = "dir", required = false) String dir) throws IOException {
         if (dir == null) {
             dir = System.getProperty("user.home");
         }
         Path dirPath = Paths.get(dir);
-        return Files.list(dirPath).toArray(Path[]::new);
+        return Files.list(dirPath)
+                .map(dirPath::relativize)
+                .map(p -> new FSItem(p, Files.isDirectory(p)))
+                .toArray(FSItem[]::new);
     }
 
 }
