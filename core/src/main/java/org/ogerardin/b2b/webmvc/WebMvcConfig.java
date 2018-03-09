feb42ac844a25b11c0a70430f982d4ab551f04fb
oge <olivier.gerardin@fundsquare.net>
2018-03-09 17:55:46 +0100

    Attempt to package a webapp in a separate module (not quite there yet)

---
+++ core/src/main/java/org/ogerardin/b2b/webmvc/WebMvcConfig.java
@@ -0,0 +1,28 @@
+package org.ogerardin.b2b.webmvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Spring MVC configuration to:
+ * -serve /webjars/** from /META-INF/resources/webjars
+ * -serve everything else from the classpath
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/public/" };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
+        if (!registry.hasMappingForPattern("/**")) {
+            registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+        }
+    }
+
+}
