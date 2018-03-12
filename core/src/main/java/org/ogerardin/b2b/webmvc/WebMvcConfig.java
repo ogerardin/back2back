506e152b1a50f8ab21a61ef7d0b8795f914ee6b5
oge <olivier.gerardin@fundsquare.net>
2018-03-12 10:46:48 +0100

    Slight modifications to make webapp module merge work

--- core/src/main/java/org/ogerardin/b2b/webmvc/WebMvcConfig.java
+++ core/src/main/java/org/ogerardin/b2b/webmvc/WebMvcConfig.java
@@ -1,7 +1,10 @@
 package org.ogerardin.b2b.webmvc;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -14,6 +17,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/public/" };
 
+/*
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         super.addResourceHandlers(registry);
@@ -23,6 +27,18 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         if (!registry.hasMappingForPattern("/**")) {
             registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
         }
+    }
+*/
+
+    @Bean
+    @Profile("dev")
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("http://localhost:8080");
+            }
+        };
     }
 
 }
