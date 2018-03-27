95a9e31903e4c5997d2cf51d0ef9b7c21354ccd8
ogerardin@yahoo.com <bebe081060>
2018-03-27 21:52:31 +0200

    Work on edit/add target

--- vuejs-ui/src/main/webapp/src/components/TargetAdd.vue
+++ vuejs-ui/src/main/webapp/src/components/TargetAdd.vue
@@ -4,24 +4,24 @@
       <h2>Add Target</h2>
     </slot>
 
-    <b-form v-on:submit="addTarget">
+    <b-form @submit="addTarget">
 
-      <b-form-group label="Type" label-for="type">
-        <b-form-select id="type" v-model="target._class" :options="options" class="mb-3" />
+      <b-form-group label="Type:" label-for="type">
+        <b-form-select id="type" required v-model="target._class" :options="options" />
       </b-form-group>
 
-      <b-form-group label="Name" label-for="name">
+      <b-form-group label="Name:" label-for="name">
         <b-form-input id="name" v-model="target.name"></b-form-input>
       </b-form-group>
 
-
       <template v-if="target._class=='.PeerTarget'">
-        <b-form-group label="Hostname" label-for="host">
-          <b-form-input id="host" v-model="target.hostname"></b-form-input>
+        <b-form-group label="Hostname:" label-for="host">
+          <b-form-input id="host" required v-model="target.hostname"></b-form-input>
         </b-form-group>
-        <b-form-group label="Port" label-for="port">
-          <b-form-input id="port" v-model="target.port"></b-form-input>
+        <b-form-group label="Port:" label-for="port">
+          <b-form-input id="port" required type="number" v-model="target.port"></b-form-input>
         </b-form-group>
+        <p>Peer URL: http://{{target.hostname}}:{{target.port}}</p>
       </template>
 
       <b-form-group>
@@ -32,6 +32,7 @@
         <b-button variant="primary" type="submit">Save</b-button>
         <b-button variant="secondary" v-on:click="cancel()">Cancel</b-button>
       </slot>
+
     </b-form>
   </b-container>
 </template>
@@ -46,6 +47,7 @@
       return {
         target: {
           _class: null,
+          port:8080,
           enabled: true,
         },
         options: [
@@ -57,15 +59,10 @@
       };
     },
     mounted() {
-      if (this.targetId) {
-        this.getTarget(this.targetId);
-      }
-      else {
-        this.getTarget(this.$route.params.id);
-      }
     },
     methods: {
-      addTarget() {
+      addTarget(evt) {
+        evt.preventDefault();
         const target = this.target;
         this.$http.post('http://localhost:8080/api/targets', target).then(response => {
           this.target = response.data;
--- vuejs-ui/src/main/webapp/src/components/TargetEdit.vue
+++ vuejs-ui/src/main/webapp/src/components/TargetEdit.vue
@@ -5,13 +5,23 @@
     </slot>
 
     <b-form v-on:submit="updateTarget">
-      <b-form-group label="ID" label-for="id">
+
+      <b-form-group label="ID:" label-for="id">
         <b-form-input id="id" v-model="target.id" readonly></b-form-input>
       </b-form-group>
 
-      <b-form-group label="Name" label-for="name">
+      <b-form-group label="Name:" label-for="name">
         <b-form-input id="name" v-model="target.name"></b-form-input>
       </b-form-group>
+
+      <template v-if="target._class=='.PeerTarget'">
+        <b-form-group label="Hostname:" label-for="host">
+          <b-form-input id="host" required v-model="target.hostname"></b-form-input>
+        </b-form-group>
+        <b-form-group label="Port:" label-for="port">
+          <b-form-input id="port" required type="number" v-model="target.port"></b-form-input>
+        </b-form-group>
+      </template>
 
       <b-form-group>
         <b-form-checkbox v-model="target.enabled">Enabled</b-form-checkbox>
@@ -21,6 +31,7 @@
         <b-button variant="primary" type="submit">Save</b-button>
         <b-button variant="secondary" v-on:click="cancel()">Cancel</b-button>
       </slot>
+
     </b-form>
   </b-container>
 </template>
