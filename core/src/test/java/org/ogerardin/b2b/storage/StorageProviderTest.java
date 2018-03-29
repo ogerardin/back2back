df8c0ba3ea439e6e8c1f615fbd41c92d6f12f80e
oge <olivier.gerardin@fundsquare.net>
2018-03-29 16:56:57 +0200

    Fixed test

--- core/src/test/java/org/ogerardin/b2b/storage/StorageProviderTest.java
+++ core/src/test/java/org/ogerardin/b2b/storage/StorageProviderTest.java
@@ -26,7 +26,9 @@ public abstract class StorageProviderTest<S extends StorageService> {
 
         // list all files in resource directory
         URL url = getClass().getResource(FILESET_RSC);
-        List<Path> paths0 = Files.list(Paths.get(url.toURI())).collect(Collectors.toList());
+        List<Path> paths0 = Files.list(Paths.get(url.toURI()))
+                .sorted()
+                .collect(Collectors.toList());
 
         // store each file in storage service
         paths0.forEach(storageService::store);
@@ -34,26 +36,20 @@ public abstract class StorageProviderTest<S extends StorageService> {
         // retrieve paths of stored files
         List<Path> paths1 = storageService.getAllFiles(true)
                 .map(FileInfo::getPath)
+                .sorted()
                 .collect(Collectors.toList());
 
+        Assert.assertEquals(paths0.size(), paths1.size());
+
         // compare each file to the stored version
-        for (Path p1 : paths1) {
-            for (Path p0 : paths0) {
-                if (p0.endsWith(p1)) {
-                    InputStream inputStream1 = storageService.getAsInputStream(p1.toString());
-                    InputStream inputStream0 = Files.newInputStream(p0, StandardOpenOption.READ);
-                    Assert.assertTrue(IOUtils.contentEquals(inputStream0, inputStream1));
-                    paths0.remove(p0);
-                    break;
-                }
-                // the listed file does not match any of the stored files
-                Assert.fail("No match for " + p1);
-            }
+        for (int i = 0; i < paths0.size(); i++) {
+            Path p0 = paths0.get(i);
+            Path p1 = paths1.get(i);
+            Assert.assertTrue(p0.endsWith(p1));
+            InputStream inputStream1 = storageService.getAsInputStream(p1.toString());
+            InputStream inputStream0 = Files.newInputStream(p0, StandardOpenOption.READ);
+            Assert.assertTrue(IOUtils.contentEquals(inputStream0, inputStream1));
         }
-
-        // make sure all the stored files have been matched
-        Assert.assertTrue(paths0.isEmpty());
-
     }
 
 }
