1e730db364331a98412c8a048d08b97fdde12fbc
oge <olivier.gerardin@fundsquare.net>
2018-08-29 17:39:52 +0200

    Progress to auto-update

---
+++ system-tray/src/main/java/org/ogerardin/b2b/update/Download.java
@@ -0,0 +1,12 @@
+package org.ogerardin.b2b.update;
+
+import lombok.Data;
+
+import java.net.URL;
+
+@Data
+public class Download {
+
+    private final URL url;
+
+}
---
+++ system-tray/src/main/java/org/ogerardin/b2b/update/Downloader.java
@@ -0,0 +1,25 @@
+package org.ogerardin.b2b.update;
+
+import lombok.Data;
+import org.apache.commons.io.FileUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+
+@Data
+public class Downloader {
+
+    private final Path dir;
+
+    public void download(Download download) throws IOException {
+        URL url = download.getUrl();
+
+        String[] parts = url.getPath().split("/");
+        String filename = parts[parts.length - 1];
+
+        Path targetFile = dir.resolve(filename);
+
+        FileUtils.copyURLToFile(url, targetFile.toFile(), 10000, 10000);
+    }
+}
