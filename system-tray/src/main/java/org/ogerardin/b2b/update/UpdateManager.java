8c2e304a1978129041c8d2811ba731d88342f5ca
oge <olivier.gerardin@fundsquare.net>
2018-09-03 17:46:39 +0200

    Progress to auto-update

--- system-tray/src/main/java/org/ogerardin/b2b/update/UpdateManager.java
+++ system-tray/src/main/java/org/ogerardin/b2b/update/UpdateManager.java
@@ -2,8 +2,11 @@ package org.ogerardin.b2b.update;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.apache.commons.io.FileUtils;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
@@ -41,15 +44,34 @@ public class UpdateManager {
     }
 
     public void update(Release release) throws IOException {
+        // download zip
         Path tempDirectory = Files.createTempDirectory(this.getClass().getSimpleName());
         Downloader downloader = new Downloader(tempDirectory);
 
         Path downloadedFile = downloader.download(release.getZipDownloadUrl());
 
-        ZipFile zipFile = new ZipFile(downloadedFile.toFile());
-        Enumeration<? extends ZipEntry> entries = zipFile.entries();
+        // unzip
+        Path outputFolder = downloadedFile.getParent().resolve("extracted");
+        unzip(downloadedFile, outputFolder);
 
+        // TODO process
 
+    }
+
+    private void unzip(Path zipFile, Path outputFolder) throws IOException {
+        ZipFile zf = new ZipFile(zipFile.toFile());
+        Enumeration<? extends ZipEntry> entries = zf.entries();
+        while (entries.hasMoreElements()) {
+            ZipEntry entry = entries.nextElement();
+            String fileName = entry.getName();
+            Path extractFile = outputFolder.resolve(fileName);
+            Files.createDirectories(extractFile.getParent());
+            try (
+                    InputStream inputStream = new BufferedInputStream(zf.getInputStream(entry))
+            ) {
+                FileUtils.copyInputStreamToFile(inputStream, extractFile.toFile());
+            }
+        }
     }
 
     private int compareVersion(Release r1, Release r2) {
