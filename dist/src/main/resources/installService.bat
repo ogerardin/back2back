b04ccf4046afd781426e1d6c85a2df9497deb13b
oge <olivier.gerardin@fundsquare.net>
2019-01-18 15:32:33 +0100

    Fixed: Windows service install

--- dist/src/main/resources/installService.bat
+++ dist/src/main/resources/installService.bat
@@ -1,10 +1,13 @@
 @echo off
+setlocal
 
 for %%i in ("%~dp0.") do SET "B2BHOME=%%~fi"
 
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set BITNESS=32 || set BITNESS=64
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set BITNESS=32|| set BITNESS=64
 set PATH=%B2BHOME%\nssm\win%BITNESS%;%PATH%
 
-nssm install @back2back.service-name@ "java -jar %B2BHOME%\@back2back.core.jar@"
-REM nssm set @back2back.service-name@ AppDirectory %B2BHOME%
+nssm install @back2back.service-name@ java -jar @back2back.core.jar@
+nssm set @back2back.service-name@ AppDirectory %B2BHOME%
 nssm start @back2back.service-name@
+
+endlocal
--- dist/src/main/resources/removeService.bat
+++ dist/src/main/resources/removeService.bat
@@ -1,8 +1,12 @@
 @echo off
+setlocal
 
 for %%i in ("%~dp0.") do SET "B2BHOME=%%~fi"
 
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set BITNESS=32 || set BITNESS=64
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set BITNESS=32|| set BITNESS=64
 set PATH=%B2BHOME%\nssm\win%BITNESS%;%PATH%
 
-nssm.exe remove @back2back.service-name@ confirm
+nssm stop @back2back.service-name@
+nssm remove @back2back.service-name@ confirm
+
+endlocal
