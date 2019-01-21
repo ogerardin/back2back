c8525e1ddd21b0cde1b8201cfb8cc9463a5ca5d2
ogerardin@yahoo.com <bebe081060>
2019-01-21 12:02:02 +0100

    Inno Setup download plugin: add header only if required

--- dist/src/main/innosetup/compile.bat
+++ dist/src/main/innosetup/compile.bat
@@ -9,7 +9,7 @@ REM Note: We use a custom Scoop Manifest as Inno Setup is not available in the d
 cmd /c scoop install inno-setup.json
 
 REM Make sure the Inno Download Plugin is installed
-REM Note: We use a custom Scoop Manifest as Inno Setup is not available in the default bucket
+REM Note: We use a custom Scoop Manifest as Inno Download Plugin is not available in the default bucket
 cmd /c scoop install inno-download-plugin.json
 
 REM add the include path to the default Inno Setup config
@@ -17,8 +17,11 @@ FOR /F "tokens=*" %%g IN ('scoop prefix inno-setup') do (SET INNO_SETUP_HOME=%%g
 FOR /F "tokens=*" %%g IN ('scoop prefix inno-download-plugin') do (SET INNO_DOWNLOAD_PLUGIN_HOME=%%g)
 echo %INNO_SETUP_HOME%
 echo %INNO_DOWNLOAD_PLUGIN_HOME%
-REM FIXME should check if already present
-echo #pragma include __INCLUDE__ + ";" + "%INNO_DOWNLOAD_PLUGIN_HOME%" >> %INNO_SETUP_HOME%\ISPPBuiltins.iss
+
+set ISSPPBUILTINS_FILE=%INNO_SETUP_HOME%\ISPPBuiltins.iss
+find /c "%INNO_DOWNLOAD_PLUGIN_HOME%" "%ISSPPBUILTINS_FILE%" || (
+    echo #pragma include __INCLUDE__ + ";" + "%INNO_DOWNLOAD_PLUGIN_HOME%" >> %ISSPPBUILTINS_FILE%
+)
 
 REM Compile the installer using the command-line compiler (ISCC.exe)
 iscc install.iss
